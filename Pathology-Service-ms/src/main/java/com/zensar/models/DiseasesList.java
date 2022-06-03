24a3d807a2fc18e7edb87bce4b9ca6c60c108bbc
kuntojijyoti <101237374+kuntojijyoti@users.noreply.github.com>
2022-06-03 12:36:35 +0530

    Add files via upload

---
+++ Pathology-Service-ms/src/main/java/com/zensar/models/DiseasesList.java
@@ -0,0 +1,23 @@
+package com.zensar.models;
+
+
+
+	import java.util.List;
+
+	public class DiseasesList {
+
+		public List<Disease> diseases;
+
+		public List<Disease> getDiseases() {
+			return diseases;
+		}
+
+		public void setDiseases(List<Disease> diseases) {
+			this.diseases = diseases;
+		}
+		
+		
+	}
+
+
+
