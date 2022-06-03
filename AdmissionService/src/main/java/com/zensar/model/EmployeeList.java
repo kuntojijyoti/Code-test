f848bc7b223133ca9e239ddaa540da978a2383a1
kuntojijyoti <101237374+kuntojijyoti@users.noreply.github.com>
2022-06-03 12:38:14 +0530

    Add files via upload

---
+++ AdmissionService/src/main/java/com/zensar/model/EmployeeList.java
@@ -0,0 +1,23 @@
+package com.zensar.model;
+
+
+
+	import java.util.List;
+
+	public class EmployeeList {
+
+		public List<Employee> employees;
+
+		public List<Employee> getEmployees() {
+			return employees;
+		}
+
+		public void setEmployees(List<Employee> employees) {
+			this.employees = employees;
+		}
+		
+		
+	}
+
+
+
